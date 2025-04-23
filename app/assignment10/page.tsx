0fb6584d144cbcb14e76c84dca7321a52ee5e2f5
minhlc1108 <minh.lcm.2003@gmail.com>
2025-04-24 00:44:30 +0700

    up

--- app/assignment10/page.tsx
+++ app/assignment10/page.tsx
@@ -1,5 +1,23 @@
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+
 export default function Assignment10() {
   return (
-     <div>Assignment10</div>
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <Card className="w-full max-w-xl shadow-xl rounded-2xl">
+        <CardHeader>
+          <CardTitle className="text-center text-xl font-bold">
+              Ứng dụng kiến thức của các tuần để hoàn thành 4 ứng dụng đơn giản
+          </CardTitle>
+        </CardHeader>
+        <CardContent className="">
+          <ul className="list-decimal w-full flex flex-col items-center justify-center">
+            <li>Caculator</li>
+            <li>Todo List</li>
+            <li>Weather App</li>
+            <li>Netflix Clone</li>
+          </ul>
+        </CardContent>
+      </Card>
+    </div>
   );
 }
--- app/assignment9/page.tsx
+++ app/assignment9/page.tsx
@@ -1,5 +1,18 @@
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+
 export default function Assignment9() {
   return (
-     <div>Assignment9</div>
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <Card className="w-full max-w-md shadow-xl rounded-2xl">
+        <CardHeader>
+          <CardTitle className="text-center text-xl font-bold">
+            Deploy WeatherApp
+          </CardTitle>
+        </CardHeader>
+        <CardContent className="text-center">
+          <a href="https://my-tailwind-project-nt8m-mbr57xlhc-minhlc1108s-projects.vercel.app/" className="w-full text-blue-700">https://my-tailwind-project-nt8m.vercel.app/</a>
+        </CardContent>
+      </Card>
+    </div>
   );
 }
