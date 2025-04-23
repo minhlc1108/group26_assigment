8155b89d413a512d5c2cda2a5745a22afc32f52d
minhlc1108 <minh.lcm.2003@gmail.com>
2025-04-23 18:58:17 +0700

    assigment 8

--- app/assignment8/page.tsx
+++ app/assignment8/page.tsx
@@ -2,26 +2,35 @@
 
 import { useState } from "react";
 import { useRouter } from "next/navigation";
+import { signIn, signOut, useSession } from "next-auth/react";
+import { toast } from "react-toastify";
 
 export default function LoginPage() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
   const router = useRouter();
-
-  const handleLogin = (e: React.FormEvent) => {
+  const { data: session } = useSession()
+  const handleLogin = async (e: React.FormEvent) => {
     e.preventDefault();
-    if (email === "user@example.com" && password === "123456") {
-      router.push("/login-success");
-    } else {
-      setError("Email hoặc mật khẩu không đúng!");
+    const res = await signIn("credentials", {
+      redirect: false,
+      email,
+      password,
+    });
+    if (res?.error) {
+      setError("Email hoặc mật khẩu không đúng");
+    }else{
+      toast.success("Đăng nhập thành công!")
     }
   };
 
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100">
       <div className="bg-white shadow-lg rounded-lg p-8 w-full max-w-md">
-        <h2 className="text-2xl font-semibold text-center mb-6">Đăng nhập</h2>
+        <h2 className="text-2xl font-semibold text-center mb-6">{session ? "Đăng nhập thành công" : "Đăng nhập"}</h2>
+        {session ? <div>Xin chào bạn {session.user?.email} <button onClick={() => signOut()} className="w-full bg-blue-500 text-white py-2 rounded hover:bg-blue-600 transition">Thoát</button></div>
+        :  
         <form onSubmit={handleLogin} className="space-y-4">
           <div>
             <label className="block mb-1 text-sm font-medium">Email</label>
@@ -51,6 +60,7 @@ export default function LoginPage() {
             Đăng nhập
           </button>
         </form>
+      }
       </div>
     </div>
   );
--- components/assignment8/login-success.tsx
+++
@@ -1,9 +0,0 @@
-export default function LoginSuccess() {
-    return (
-      <div className="text-center">
-        <h1 className="text-3xl font-bold text-green-600 mb-2">✅ Đăng nhập thành công</h1>
-        <p className="text-lg text-gray-700">Chào mừng bạn đã vào hệ thống.</p>
-      </div>
-    );
-  }
-  
